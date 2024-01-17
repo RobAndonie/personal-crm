d0288be5d46004a9be90e43d91fc9c2d967f659c
RobAndonie <a01411863@tec.mx>
2024-01-17 14:16:38 -0600

    Frontend setup complete

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // adjust this if you're using TypeScript or different file structures
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
